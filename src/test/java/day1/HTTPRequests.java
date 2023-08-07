6daa5dad52cc0ae3698b88e05d85acea8e9e894d
Rohit Singh Bhandari <rohuu99022@gmail.com>
2023-08-07 18:27:05 +0530

    tests for update user and delete user

--- src/test/java/day1/HTTPRequests.java
+++ src/test/java/day1/HTTPRequests.java
@@ -1,5 +1,83 @@
 package day1;
 
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
+import io.restassured.matcher.RestAssuredMatchers.*;
+import org.testng.annotations.Test;
+import java.util.HashMap;
+import java.util.Map;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+public class HTTPRequests {
+    int id;
+    @Test(priority = 1)
+    void shouldBeAbleToGetAllUsers(){
+        given()
+
+                .when()
+                    .get("https://reqres.in/api/users?page=2")
+                .then()
+                    .statusCode(200)
+                    .body("page", equalTo(2))
+                    .log().all();
+    }
+
+    @Test(priority = 2)
+    void shouldBeAbleToCreateNewUser(){
+        Map<String, String> map=new HashMap<>();
+        map.put("name","Morpheus");
+        map.put("job","leader");
+
+         id= given()
+                .contentType("application/json")
+                .body(map)
+
+                .when()
+                    .post("https://reqres.in/api/users")
+                .jsonPath().getInt("id");
+
+//                .then()
+//                    .statusCode(201)
+//                    .log().all();
+    }
+
+    @Test(priority = 3, dependsOnMethods = ("shouldBeAbleToCreateNewUser"))
+    void shouldBeAbleToUpdateTheUser(){
+        Map<String, String> updatedData=new HashMap<>();
+        updatedData.put("name","RohitS");
+        updatedData.put("job","plumber");
+
+        given()
+                .contentType("application/json")
+                .body(updatedData)
+
+                .when()
+                        .put("https://reqres.in/api/users/"+id)
+
+                .then()
+                .statusCode(200)
+                .log().all();
+    }
+
+    @Test(priority = 4)
+    void shouldBeAbleToDeleteUser(){
+        given()
+
+                .when()
+                .delete("https://reqres.in/api/users/"+id)
+
+                .then()
+                .statusCode(204)
+                .log().all();
+    }
+}
+
+
+
+
+
 /*
     given()
         // content type, set cookies, add auth, add params, set headers info etc
@@ -28,50 +106,3 @@ UPDATE USER: https://reqres.in/api/users/2
                   }
  */
 
-import io.restassured.matcher.ResponseAwareMatcher;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.Test;
-import io.restassured.RestAssured.*;
-import io.restassured.matcher.RestAssuredMatchers.*;
-import org.hamcrest.Matchers.*;
-
-import java.util.HashMap;
-import java.util.Map;
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.testng.TestRunner.PriorityWeight.priority;
-
-
-public class HTTPRequests {
-    int id;
-    @Test
-    void getAllUsers(){
-        given()
-
-                .when()
-                    .get("https://reqres.in/api/users?page=2")
-                .then()
-                    .statusCode(200)
-                    .body("page", equalTo(2))
-                    .log().all();
-    }
-
-    @Test
-    void createNewUser(){
-        Map<String, String> map=new HashMap<>();
-        map.put("name","Morpheus");
-        map.put("job","leader");
-
-        given()
-                .contentType("application/json")
-                .body(map)
-
-                .when()
-                    .post("https://reqres.in/api/users")
-
-                .then()
-                    .statusCode(201)
-                    .log().all();
-    }
-
-}
